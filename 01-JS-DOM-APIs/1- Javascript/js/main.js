738813c587e70c4785ad29cef53fb0cdf469e469
langStrife <langstrife@gmail.com>
2017-04-27 17:04:33 -0300

    First js Commit
    Error getting json from data.json.
    I'm uploading to git so the coordinators may help me

---
+++ 01-JS-DOM-APIs/1- Javascript/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", init);
+
+window.onload = function () {
+	document.getElementsByClassName("sec")[0].style.visibility = "visible";
+}
+
+var button = document.getElementsByClassName("jokes-button")[0];
+
+function init() {
+	button.addEventListener('click', getData);
+}
+
+function getData() {
+	function reqListener (){
+		var json = {};
+		try{
+			json = JSON.parse(this.responseText);
+		} catch (e) {}
+
+		console.log(json);
+		attachSection(json);
+	}
+	var xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.addEventListener("load", reqListener);
+	xmlhttp.open("GET", "data.json");
+	xmlhttp.send();
+}
+
+function attachSection(json) {
+	var para = document.createElement("P");
+	var section = document.querySelector("section");
+	para.innerHTML =json;
+	document.querySelector("section").appendChild(para);    
+}
